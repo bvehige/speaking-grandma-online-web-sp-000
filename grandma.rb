283b430e4d76627433cd32d3427167f452effabe
Brian Vehige <brianvehige@gmail.com>
2020-02-15 20:43:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(statement)
-  if (statement)   
-  
+  if (statement) == (statement).upcase
+    return "No, NOT SINCE 1938!"
+  elsif (statement) == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKING!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
+    
 end
 
 # Whatever you say to grandma, she should respond with
